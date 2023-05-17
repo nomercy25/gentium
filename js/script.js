e14b94a53baadcd544e603d89d09f60ecee60d03
nomercy25 <tema.polivoda@gmail.com>
2023-05-17 12:13:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+new Swiper ('.swiper', {
+        //arrows 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false
+    },
+
+        loop: true,
+         SlidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loopedSlides: 3,
+
+    //navigation pagination
+    pagination: {
+        el: '.swiper-pagination',
+        //bullets
+        clickable: true,
+    },
+    simulateTouch: true,
+    grabCursor: true,
+    slideToClickedSlide: true,
+    autoHeight: true,
+    slidesPerView: 'auto',
+    breakpoints: {
+
+        320: {
+        slidesPerView: 'auto',
+        slidesPerGroup: 1,
+        loopedSlides: 3,
+        },
+
+        480: {
+            slidesPerView: 'auto',
+            slidesPerGroup: 1,
+            loopedSlides: 2,
+        },
+
+        768: {
+            slidesPerView: 'auto',
+            slidesPerGroup: 1,
+            loopedSlides: 2,
+        },
+
+        992: {
+            slidesPerView: 3,
+        },
+
+        1024: {
+          slidesPerView: 'auto',
+          spaceBetween: 0,
+        }
+      },
+});
